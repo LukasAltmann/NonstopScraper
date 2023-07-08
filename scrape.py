86bad559f6c5873e2c262e9e2d279c20aae12bba
Lukas <lukasaltmann10@gmail.com>
2023-07-08 16:30:31 +0200

    Added scraping too through selenium

--- scrape.py
+++ scrape.py
@@ -1,96 +1,36 @@
-import csv
-import json
+import time
 
-import requests
-from bs4 import BeautifulSoup
-from classes import Event
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 
-def translate_file(filename):
+def scrape():
+    def write_to_file(name, content):
+        f = open("input/" + name, "w")
+        f.write(content)
+        f.close()
 
-    cinemas = ['Admiral Kino', 'Burg Kino', 'De France', 'Filmcasino', 'Filmhaus Kino Spittelberg', 'Gartenbaukino',
-               'Schikaneder',
-               'Stadtkino im KÃ¼nstlerhaus', 'Top Kino', 'Votiv Kino']
+    def create_filename(input_text):
+        parts = input_text.split(' ')[1].replace('.', '_').removesuffix('_').split('_')
+        return parts[1] + '_' + parts[0] + '.html'
 
-    cinemas_dict = {'Admiral Kino': [],
-                    'Burg Kino': [],
-                    'De France': [],
-                    'Filmcasino': [],
-                    'Filmhaus Kino Spittelberg': [],
-                    'Gartenbaukino': [],
-                    'Schikaneder': [],
-                    'Stadtkino im KÃ¼nstlerhaus': [],
-                    'Top Kino': [],
-                    'Votiv Kino': []}
+    options = Options()
+    options.add_argument('--headless=new')
+    driver = webdriver.Chrome(options=options)
 
-    def filter_items(events):
-        clean_events = []
-        for event in events:
-            if event.place in cinemas:
-                clean_events.append(event)
+    driver.get('https://www.film.at/kinoprogramm/wien')
 
-                matching = event.place
+    select = Select(driver.find_element(By.ID, 'input0'))
 
-                match matching:
-                    case 'Admiral Kino':
-                        cinemas_dict['Admiral Kino'].append(event)
-                    case 'Burg Kino':
-                        cinemas_dict['Burg Kino'].append(event)
-                    case 'De France':
-                        cinemas_dict['De France'].append(event)
-                    case 'Filmcasino':
-                        cinemas_dict['Filmcasino'].append(event)
-                    case 'Filmhaus Kino Spittelberg':
-                        cinemas_dict['Filmhaus Kino Spittelberg'].append(event)
-                    case 'Gartenbaukino':
-                        cinemas_dict['Gartenbaukino'].append(event)
-                    case 'Schikaneder':
-                        cinemas_dict['Schikaneder'].append(event)
-                    case 'Stadtkino im KÃ¼nstlerhaus':
-                        cinemas_dict['Stadtkino im KÃ¼nstlerhaus'].append(event)
-                    case 'Top Kino':
-                        cinemas_dict['Top Kino'].append(event)
-                    case 'Votiv Kino':
-                        cinemas_dict['Votiv Kino'].append(event)
+    upper_bound = 14
+    if len(select.options) - 1 < upper_bound:
+        upper_bound = len(select.options) - 1
 
-        return clean_events
+    for index in range(0, upper_bound):
+        select.select_by_index(index)
+        filename = create_filename(select.options[index].text)
+        write_to_file(filename, driver.page_source)
 
-    # Adresse der Webseite
-    url = 'https://www.film.at/kinoprogramm/wien'
-    headers = {'Accept-Language': 'de-DE;q=0.8,de;q=0.7'}
 
-    # GET-Request ausführen
-    response = requests.get(url, headers=headers)
-
-    # BeautifulSoup HTML-Dokument aus dem Quelltext parsen
-    # html2 = BeautifulSoup(response.text, 'html.parser')
-
-    with open('./input/' + filename) as fp:
-        html = BeautifulSoup(fp, 'html.parser')
-
-    movie_accordeons = html.find_all('accordeon', class_='accordeon ng-star-inserted')
-
-    items = []
-    for accordeon in movie_accordeons:
-
-        title = accordeon.find('div', class_='accordeon-title').text.strip()
-        events = accordeon.find_all('div', class_='filterList-results-item ng-star-inserted')
-
-        event_items = []
-        for event in events:
-            event_location = event.find('div', class_='filterList-results-key').text.strip()
-            event_time = event.find('div', class_='filterList-results-value').text.strip()
-            event_items.append(Event(title, event_location, event_time))
-
-        event_items_cleaned = filter_items(event_items)
-
-        if len(event_items_cleaned) >= 1:
-            items.extend(event_items_cleaned)
-
-    with open('output/' + filename.replace('html', 'json'), 'w') as outfile:
-        outfile.write('[')
-        for event in items:
-            outfile.write(json.dumps(event.__dict__, ensure_ascii=False))
-            if event != items[-1]:
-                outfile.write(',')
-        outfile.write(']')
